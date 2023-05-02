308c357351bb0ad69a1e360b890190de8a40f7f3
Thixtor <sebas.sas@gmail.com>
2023-05-02 15:53:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+var nombre = 'Sebastian';
+
+var saludo = <p>
+                Hola mundo soy {nombre} <br />
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        { saludo }
         <a
           className="App-link"
           href="https://reactjs.org"
