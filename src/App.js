34d688b705922a98ccaa1d3fd4ba3cafeb947150
Thixtor <sebas.sas@gmail.com>
2023-05-04 15:47:02 -0500

    Funcion del boton exportar
    Se agrega la funcionalidad del boton exportar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
+import html2canvas from 'html2canvas';
 
 function App() {
 
@@ -19,6 +20,20 @@ function App() {
     setImagen(evento.target.value)
   }
 
+  const onClickExportar = function(evento) {
+    alert('Exportar')
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      
+      var img = canvas.toDataURL('image/png');
+
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+  });
+
+  }
+
   return (
     <div className="App">
       
@@ -33,9 +48,10 @@ function App() {
 
       <input onChange={onChangeLinea1} type='text' placeholder='Linea 1' /> <br />
       <input onChange={onChangeLinea2} type='text' placeholder='Linea 2'/> <br />
-      <button>Exportar</button>
 
-      <div>
+      <button onClick={onClickExportar}>Exportar</button>
+
+      <div className='meme' id='meme'>
         <span>{linea1}</span> <br />
         <span>{linea2}</span>
         <img src={'/memes/' + imagen + '.jpg'} />
